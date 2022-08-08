e190829b05e6a3f684f79641f16d52ad5ca10f22
DaryaShonoeva <43034048+DaryaShonoeva@users.noreply.github.com>
2022-08-08 16:36:13 +0300

    Update UserServiceImpl.java

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl ud = new UserDaoJDBCImpl();
+    UserDaoJDBC ud = new UserDaoJDBCImpl();
 
 
     public UserServiceImpl() throws SQLException {
